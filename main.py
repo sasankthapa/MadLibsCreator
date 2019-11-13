c7468565b53df9d90bee099a9db6938f01d12ed2
sasankthapa0 <thapas@beloit.edu>
2019-11-13 10:40:07 -0600

    Now able to process tags. tags returns what type of word we want from user.

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
-import nltk
+import nltk, random
 import os,sys
+from nltk.tokenize import sent_tokenize
+from nltk.tokenize import word_tokenize
+from nltk.corpus import stopwords
+
+def initializeStopWords():
+    global stop_words
+    stop_words=set(stopwords.words("english"))
+
+tags_data={'NN':"Singular Noun",'NNS':"Plural Noun",'NNP':"Proper Noun (Harrison)",'NNPS':"Plural Proper Noun (Americans)",'JJ':"Adjective",'JJR':"Comparitive Adjective (bigger)",'JJS':"Superlative Adjective (biggest)",'RB':"Adverb",'RBR':"Adverb Compative",'RBS':"Adverb Superlative",'VB':'Verb','VBD':"Verb past tense",'VBG':"Verb Continous",'VBN':"Verb Past",'VBP':"Verb",'VBZ':"Verbs 3rd person (sings, takes)"}
+def processTag(tag):
+    return tags_data.get(tag)
 
 filename=input("Enter name of file:  ")
+initializeStopWords()
+print(stop_words)
 
 if not os.path.isfile(filename):
     print("error filenot found")
@@ -10,4 +23,22 @@ if not os.path.isfile(filename):
 file=open(filename,'r')
 lines=file.readlines()
 lines=[line.strip() for line in lines]
-print(lines)
+
+for line in lines:
+    print(line)
+    x=sent_tokenize(line)
+    for a in x:
+        choices=[]
+        words=word_tokenize(a)
+        words=[word for word in words if word not in stop_words]
+        tagged=nltk.pos_tag(words)
+        for taggedWord in tagged:
+            tag=taggedWord[1][:1]
+            if(tag=='N' or tag=='V' or  tag=='J' or tag=='R' and tag!='RP'):
+                choices.append(taggedWord)
+        print("x",choices)
+    if(len(choices)==0):
+        continue
+    choice=random.choice(choices)
+    print(choice)
+    print("y",processTag(choice[1]))
